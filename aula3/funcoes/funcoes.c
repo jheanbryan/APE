b36b4e57eb926ee06b122e1dbb7ee23128faaed4
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-08-25 17:17:02 -0400

    updates

---
+++ aula3/funcoes/funcoes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float calcularMassa(float a, float b) {
+    printf("%f", a * b);
+}
+
+int main() {
+    float a = 1;
+    float b = 3;
+
+   calcularMassa(a, b);
+}
---
+++ aula4/struct.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    struct carro{
+        char modelo[20];
+        int ano;
+        float valor;
+    };
+    
+}
